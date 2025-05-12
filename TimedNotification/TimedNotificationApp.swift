96c1e9a0a9e09845b35c59fad17fbdcd85ebabab
sibisai <u.sibisai@gmail.com>
2025-05-12 14:52:17 -0700

    Initial Commit

---
+++ TimedNotification/TimedNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimedNotificationApp.swift
+//  TimedNotification
+//
+//  Created by Sibi on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TimedNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
